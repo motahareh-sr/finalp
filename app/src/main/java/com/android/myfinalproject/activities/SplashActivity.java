9399ce35057647eb78d318567a14f6b426650583
motahareh-sr <motahareh.srad1381@gmail.com>
2021-12-31 13:05:42 +0330

    first commit

---
+++ app/src/main/java/com/android/myfinalproject/activities/SplashActivity.java
@@ -0,0 +1,47 @@
+package com.android.myfinalproject.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.myfinalproject.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_SCREEN = 5000;
+    Animation topAnim,bottomAnim;
+    ImageView image;
+    TextView logo,slogan;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+    topAnim= AnimationUtils.loadAnimation(this, R.anim.top_anim);
+    bottomAnim= AnimationUtils.loadAnimation(this, R.anim.bottom_anim);
+
+    image=findViewById(R.id.imageView4);
+    logo=findViewById(R.id.textView);
+    slogan=findViewById(R.id.textView2);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        slogan.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+        @Override
+        public void run() {
+            Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    },SPLASH_SCREEN);
+    }
+}
